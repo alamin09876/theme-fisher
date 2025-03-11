a4be33e35b02a8731de1f3e4d7b394303c1c5c02
alamin09876 <alamin.ahmed0795@gmail.com>
2025-03-11 22:20:37 +0600

    contact us done

--- src/app/components/Home/Contact.tsx
+++ src/app/components/Home/Contact.tsx
@@ -23,110 +23,135 @@ export default function Contact() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="max-w-3xl w-full mx-auto my-10 md:my-20 p-6 md:p-8 bg-white shadow-lg rounded-lg">
-        <form onSubmit={handleSubmit} className="space-y-6">
-          {/* Two-Column Layout */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {/* Column 1: First & Last Name */}
-            <div className="space-y-6">
-              <div>
-                <label className="block text-sm font-semibold text-gray-700 mb-2">
-                  First Name<span className="text-red-500">*</span>
-                </label>
-                <input
-                  type="text"
-                  name="firstName"
-                  required
-                  value={formData.firstName}
-                  onChange={handleChange}
-                  className="w-full p-3 border border-gray-300 rounded-md 
-                            focus:border-blue-500 focus:ring-2 focus:ring-blue-200
-                            transition-all duration-200"
-                />
+    <div className="m-4 grid lg:grid-cols-12 col-span-1  gap-4">
+      <div className="lg:col-span-4 "></div>
+      <div className="lg:col-span-8 ">
+        <div className="text-white mb-3 text-3xl pl-10">
+          <p>Skills</p>
+        </div>
+        <div className=" flex  ">
+          <div className="max-w-3xl w-full mx-auto ">
+            <form onSubmit={handleSubmit}>
+              {/* Two-Column Layout */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {/* Column 1: First & Last Name */}
+                <div className="space-y-6">
+                  <div className="relative">
+                    <label className="block text-sm font-semibold text-white mb-2">
+                      First Name<span className="text-white">*</span>
+                    </label>
+                    <div
+                      className="w-full rounded-md transition-all duration-200 
+              focus-within:bg-gradient-to-r focus-within:from-[#F5BD4D] focus-within:to-[#F89222] p-[2px]"
+                    >
+                      <input
+                        type="text"
+                        name="firstName"
+                        required
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        className="w-full p-2 border border-transparent bg-black rounded-md 
+                     focus:outline-none transition-all duration-200"
+                      />
+                    </div>
+                  </div>
+                  <div className="relative">
+                    <label className="block text-sm font-semibold text-white mb-2">
+                      Email<span className="text-white">*</span>
+                    </label>
+                    <div
+                      className="w-full rounded-md transition-all duration-200 
+              focus-within:bg-gradient-to-r focus-within:from-[#F5BD4D] focus-within:to-[#F89222] p-[2px]"
+                    >
+                      <input
+                        type="email"
+                        name="email"
+                        required
+                        value={formData.email}
+                        onChange={handleChange}
+                        className="w-full p-2 border border-transparent bg-black rounded-md 
+                     focus:outline-none transition-all duration-200"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                {/* Column 2: Email & Phone */}
+                <div className="space-y-6">
+                  <div className="relative">
+                    <label className="block text-sm font-semibold text-white mb-2">
+                      Last Name<span className="text-white">*</span>
+                    </label>
+                    <div
+                      className="w-full rounded-md transition-all duration-200 
+              focus-within:bg-gradient-to-r focus-within:from-[#F5BD4D] focus-within:to-[#F89222] p-[2px]"
+                    >
+                      <input
+                        type="text"
+                        name="lastName"
+                        required
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        className="w-full p-2 border border-transparent bg-black rounded-md 
+                     focus:outline-none transition-all duration-200"
+                      />
+                    </div>
+                  </div>
+                  <div className="relative">
+                    <label className="block text-sm font-semibold text-white mb-2">
+                      Phone<span className="text-white">*</span>
+                    </label>
+                    <div
+                      className="w-full rounded-md transition-all duration-200 
+              focus-within:bg-gradient-to-r focus-within:from-[#F5BD4D] focus-within:to-[#F89222] p-[2px]"
+                    >
+                      <input
+                        type="tel"
+                        name="phone"
+                        required
+                        value={formData.phone}
+                        onChange={handleChange}
+                        className="w-full p-2 border border-transparent bg-black rounded-md 
+                     focus:outline-none transition-all duration-200"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
 
-              <div>
-                <label className="block text-sm font-semibold text-gray-700 mb-2">
-                  Last Name<span className="text-red-500">*</span>
+              {/* Full-Width Message Input */}
+              <div className="relative mt-6">
+                <label className="block text-sm font-semibold text-white mb-2">
+                  Message<span className="text-white">*</span>
                 </label>
-                <input
-                  type="text"
-                  name="lastName"
-                  required
-                  value={formData.lastName}
-                  onChange={handleChange}
-                  className="w-full p-3 border border-gray-300 rounded-md 
-                            focus:border-blue-500 focus:ring-2 focus:ring-blue-200
-                            transition-all duration-200"
-                />
-              </div>
-            </div>
-
-            {/* Column 2: Email & Phone */}
-            <div className="space-y-6">
-              <div>
-                <label className="block text-sm font-semibold text-gray-700 mb-2">
-                  Email<span className="text-red-500">*</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  required
-                  value={formData.email}
-                  onChange={handleChange}
-                  className="w-full p-3 border border-gray-300 rounded-md 
-                            focus:border-blue-500 focus:ring-2 focus:ring-blue-200
-                            transition-all duration-200"
-                />
+                <div
+                  className="w-full rounded-md transition-all duration-200 
+          focus-within:bg-gradient-to-r focus-within:from-[#F5BD4D] focus-within:to-[#F89222] p-[2px] mb-10"
+                >
+                  <textarea
+                    name="message"
+                    required
+                    rows={4}
+                    value={formData.message}
+                    onChange={handleChange}
+                    className="w-full p-2  bg-black rounded-md 
+                 focus:outline-none resize-y transition-all duration-200"
+                  />
+                </div>
               </div>
 
-              <div>
-                <label className="block text-sm font-semibold text-gray-700 mb-2">
-                  Phone<span className="text-red-500">*</span>
-                </label>
-                <input
-                  type="tel"
-                  name="phone"
-                  required
-                  value={formData.phone}
-                  onChange={handleChange}
-                  className="w-full p-3 border border-gray-300 rounded-md 
-                            focus:border-blue-500 focus:ring-2 focus:ring-blue-200
-                            transition-all duration-200"
-                />
-              </div>
-            </div>
+              {/* Submit Button */}
+              <button
+                type="submit"
+                className="w-full bg-gradient-to-r from-[#F5BD4D] to-[#F89222] 
+             hover:opacity-90 text-white px-6 py-3 rounded-md font-semibold
+             transition-all duration-200 focus:ring-2 focus:ring-[#F89222] focus:ring-offset-2 mb-10"
+              >
+                Send Message
+              </button>
+            </form>
           </div>
-
-          {/* Full-Width Message Input */}
-          <div>
-            <label className="block text-sm font-semibold text-gray-700 mb-2">
-              Message<span className="text-red-500">*</span>
-            </label>
-            <textarea
-              name="message"
-              required
-              rows={4}
-              value={formData.message}
-              onChange={handleChange}
-              className="w-full p-3 border border-gray-300 rounded-md 
-                        focus:border-blue-500 focus:ring-2 focus:ring-blue-200
-                        resize-y transition-all duration-200"
-            />
-          </div>
-
-          {/* Submit Button */}
-          <button
-            type="submit"
-            className="w-full bg-blue-600 hover:bg-blue-700 text-white 
-                      px-6 py-3 rounded-md font-semibold
-                      transition-colors duration-200
-                      focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"
-          >
-            Send Message
-          </button>
-        </form>
+        </div>
       </div>
     </div>
   );
