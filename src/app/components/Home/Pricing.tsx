e891fd8c2ffad1842207fe257cc4dfd7d24e5a42
alamin09876 <alamin.ahmed0795@gmail.com>
2025-03-13 02:01:20 +0600

    pricing done

--- src/app/components/Home/Pricing.tsx
+++ src/app/components/Home/Pricing.tsx
@@ -1,15 +1,66 @@
 const Pricing = () => {
-  const crossedFeatures = {
-    Intro: ["Interface Design", "Front-end Code", "Back-end Development"],
-    Base: ["Search Engine Optimization", "Awesome Support"],
-    Popular: ["Easy in use", "Useful site"],
-  };
-
-  const getLineThrough = (plan: string, feature: string) => {
-    if (plan === "Enterprise") return false;
-    const features = crossedFeatures[plan] || [];
-    return features.includes(feature);
-  };
+  const cardPackage = [
+    {
+      name: "Intro",
+      price: 29,
+      feature: [
+        { title: "Interface Design", isCrossed: true },
+        { title: "Front-end Code", isCrossed: false },
+        { title: "Back-end Development", isCrossed: true },
+        { title: "Search Engine Optimization", isCrossed: true },
+        { title: "Awesome Support", isCrossed: true },
+        { title: "Responsive site", isCrossed: false },
+        { title: "Easy in use", isCrossed: true },
+        { title: "Useful site", isCrossed: true },
+        { title: "Speedy web", isCrossed: true },
+      ],
+    },
+    {
+      name: "Base",
+      price: 39,
+      feature: [
+        { title: "Interface Design", isCrossed: true },
+        { title: "Front-end Code", isCrossed: false },
+        { title: "Back-end Development", isCrossed: true },
+        { title: "Search Engine Optimization", isCrossed: true },
+        { title: "Awesome Support", isCrossed: true },
+        { title: "Responsive site", isCrossed: false },
+        { title: "Easy in use", isCrossed: true },
+        { title: "Useful site", isCrossed: true },
+        { title: "Speedy web", isCrossed: true },
+      ],
+    },
+    {
+      name: "Popular",
+      price: 99,
+      feature: [
+        { title: "Interface Design", isCrossed: true },
+        { title: "Front-end Code", isCrossed: false },
+        { title: "Back-end Development", isCrossed: true },
+        { title: "Search Engine Optimization", isCrossed: true },
+        { title: "Awesome Support", isCrossed: true },
+        { title: "Responsive site", isCrossed: false },
+        { title: "Easy in use", isCrossed: true },
+        { title: "Useful site", isCrossed: true },
+        { title: "Speedy web", isCrossed: true },
+      ],
+    },
+    {
+      name: "Enterprise",
+      price: 129,
+      feature: [
+        { title: "Interface Design", isCrossed: true },
+        { title: "Front-end Code", isCrossed: false },
+        { title: "Back-end Development", isCrossed: true },
+        { title: "Search Engine Optimization", isCrossed: true },
+        { title: "Awesome Support", isCrossed: true },
+        { title: "Responsive site", isCrossed: false },
+        { title: "Easy in use", isCrossed: true },
+        { title: "Useful site", isCrossed: true },
+        { title: "Speedy web", isCrossed: true },
+      ],
+    },
+  ];
 
   return (
     <div className="mx-0 my-4 lg:m-4 grid lg:grid-cols-12 col-span-1 gap-4">
@@ -20,154 +71,40 @@ const Pricing = () => {
           <h2 className="text-3xl text-white py-10 font-extrabold">Pricing</h2>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 px-4 lg:px-11">
-          <div className="lg:col-span-6 bg-black w-full">
-            <h2 className="text-2xl text-white  border-l-0 rounded-r-4xl w-[130px] p-2 mt-10 bg-[#4A2F0E]">
-              Intro
-            </h2>
-            <div className="w-full mx-auto bg-black p-4 lg:p-8">
-              <h2 className="text-4xl font-bold text-white mb-8">
-                $29
-                <span className="text-lg font-normal text-white">/month</span>
-              </h2>
-              <ul className="grid gap-4 mb-10 bg-[#121414] text-white p-4 lg:p-7">
-                {[
-                  "Interface Design",
-                  "Front-end Code",
-                  "Back-end Development",
-                  "Search Engine Optimization",
-                  "Awesome Support",
-                  "Responsive site",
-                  "Easy in use",
-                  "Useful site",
-                  "Speedy web",
-                ].map((feature) => (
-                  <li
-                    key={feature}
-                    className={`text-white ${
-                      getLineThrough("Intro", feature)
-                        ? "line-through opacity-50"
-                        : ""
-                    }`}
-                  >
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-              <button className="w-full lg:w-[150px] border border-[#F89222] py-4 px-6 hover:bg-gradient-to-r from-[#F5BD4D] to-[#F89222]  text-white rounded-lg font-semibold transition-colors">
-                Get Started
-              </button>
-            </div>
-          </div>
-
-          <div className="lg:col-span-6 bg-black w-full">
-            <h2 className="text-2xl text-white border-l-0 rounded-r-4xl w-[130px] p-2 mt-10 bg-[#4A2F0E]">
-              Base
-            </h2>
-            <div className="w-full mx-auto bg-black p-4 lg:p-8">
-              <h2 className="text-4xl font-bold text-white mb-8">
-                $49
-                <span className="text-lg font-normal text-white">/month</span>
-              </h2>
-              <ul className="grid gap-4 mb-10 bg-[#121414] text-white p-4 lg:p-7">
-                {[
-                  "Interface Design",
-                  "Front-end Code",
-                  "Back-end Development",
-                  "Search Engine Optimization",
-                  "Awesome Support",
-                  "Responsive site",
-                  "Easy in use",
-                  "Useful site",
-                  "Speedy web",
-                ].map((feature) => (
-                  <li
-                    key={feature}
-                    className={`text-white ${
-                      getLineThrough("Base", feature)
-                        ? "line-through opacity-50"
-                        : ""
-                    }`}
-                  >
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-              <button className="w-full lg:w-[150px] border border-[#F89222] py-4 px-6 hover:bg-gradient-to-r from-[#F5BD4D] to-[#F89222]  text-white rounded-lg font-semibold transition-colors">
-                Get Started
-              </button>
-            </div>
-          </div>
-
-          <div className="lg:col-span-6 bg-black w-full">
-            <h2 className="text-2xl text-white  border-l-0 rounded-r-4xl w-[130px] p-2 mt-10 bg-[#4A2F0E] ">
-              Popular
-            </h2>
-            <div className="w-full mx-auto bg-black p-4 lg:p-8">
-              <h2 className="text-4xl font-bold text-white mb-8">
-                $99
-                <span className="text-lg font-normal text-white">/month</span>
-              </h2>
-              <ul className="grid gap-4 mb-10 bg-[#121414] text-white p-4 lg:p-7">
-                {[
-                  "Interface Design",
-                  "Front-end Code",
-                  "Back-end Development",
-                  "Search Engine Optimization",
-                  "Awesome Support",
-                  "Responsive site",
-                  "Easy in use",
-                  "Useful site",
-                  "Speedy web",
-                ].map((feature) => (
-                  <li
-                    key={feature}
-                    className={`text-white ${
-                      getLineThrough("Popular", feature)
-                        ? "line-through opacity-50"
-                        : ""
-                    }`}
-                  >
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-              <button className="w-full lg:w-[150px] border border-[#F89222] py-4 px-6 hover:bg-gradient-to-r from-[#F5BD4D] to-[#F89222]  text-white rounded-lg font-semibold transition-colors">
-                Get Started
-              </button>
-            </div>
-          </div>
-
-          <div className="lg:col-span-6 bg-black w-full">
-            <h2 className="text-2xl text-white border-l-0 rounded-r-4xl w-[130px] p-2 mt-10 bg-[#4A2F0E]">
-              Enterprise
-            </h2>
-            <div className="w-full mx-auto bg-black p-4 lg:p-8">
-              <h2 className="text-4xl font-bold text-white mb-8">
-                $129
-                <span className="text-lg font-normal text-white">/month</span>
-              </h2>
-              <ul className="grid gap-4 mb-10 bg-[#121414] text-white p-4 lg:p-7">
-                {[
-                  "Interface Design",
-                  "Front-end Code",
-                  "Back-end Development",
-                  "Search Engine Optimization",
-                  "Awesome Support",
-                  "Responsive site",
-                  "Easy in use",
-                  "Useful site",
-                  "Speedy web",
-                ].map((feature) => (
-                  <li key={feature} className="text-white  ">
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-              <button className="w-full lg:w-[150px] border border-[#F89222] py-4 px-6 hover:bg-gradient-to-r from-[#F5BD4D] to-[#F89222]  text-white rounded-lg font-semibold transition-colors">
-                Get Started
-              </button>
-            </div>
-          </div>
+          {cardPackage.map((item, idx) => {
+            return (
+              <div key={idx} className="lg:col-span-6 bg-black w-full">
+                <h2 className="text-2xl text-white  border-l-0 rounded-r-4xl w-[130px] p-2 mt-10 bg-[#4A2F0E]">
+                  {item?.name}
+                </h2>
+                <div className="w-full mx-auto bg-black p-4 lg:p-8">
+                  <h2 className="text-4xl font-bold text-white mb-8">
+                    {item?.price}
+                    <span className="text-lg font-normal text-white">
+                      /month
+                    </span>
+                  </h2>
+                  <ul className="grid gap-4 mb-10 bg-[#121414] text-white p-4 lg:p-7">
+                    {item?.feature?.map((features, idx) => (
+                      <li
+                        key={idx}
+                        className={`text-white ${
+                          features.isCrossed == true
+                            ? "line-through opacity-50"
+                            : ""
+                        }`}
+                      >
+                        {features?.title}
+                      </li>
+                    ))}
+                  </ul>
+                  <button className="w-full lg:w-[150px] border border-[#F89222] py-4 px-6 hover:bg-gradient-to-r from-[#F5BD4D] to-[#F89222]  text-white rounded-lg font-semibold transition-colors">
+                    Get Started
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
